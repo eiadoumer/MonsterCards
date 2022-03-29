06c7bf50f7ab75447b6a883d5dd8d8fde32b2bf2
Eiad Oumer <eiadoumer14@gmail.com>
2022-03-29 19:26:47 +0200

    Added Card Component and used filtering onSearch

---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import "./card-list.css"
+
+import {Card} from './card/card.component.jsx'
+
+
+
+export const CardList = (  props)=>  (
+
+<div className="card-list"> 
+
+{props.monsters.map(monster=>(<Card key={monster.id} monster={monster} /> ))}
+
+
+</div> )
+
+
+
+
+
+
